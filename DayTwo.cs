81ed478ea487845f2ee1eb31de9464cde444ec1b
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-06-30 21:45:35 +0530

    Day Two

---
+++ DayTwo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank30Day
+{
+    class DayTwo
+    {
+        static void Main(string[] args)
+        {
+            double mealCost;
+            int tipPersent;
+            int taxPersent;
+            int totalCost;
+            mealCost = Convert.ToDouble(Console.ReadLine());
+            tipPersent = Convert.ToInt16(Console.ReadLine());
+            taxPersent = Convert.ToInt16(Console.ReadLine());
+            totalCost = Convert.ToInt16(mealCost + ((mealCost * tipPersent) / 100) + ((mealCost * taxPersent) / 100));
+            Console.WriteLine("The total meal cost is " + totalCost + " dollars.");
+        }
+    }
+}
