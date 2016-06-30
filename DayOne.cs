bf4be988a0b6e9440d7b90b30f9544a3dcdfec81
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-06-30 21:40:51 +0530

    Day One

---
+++ DayOne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HackerRank30Day
+{
+    class DayOne
+    {
+        static void Main(String[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+            int k;
+            double j = 4.0;
+            string r;
+            // Declare second integer, double, and String variables.
+            k = Convert.ToInt16(Console.ReadLine());
+            //j = Convert.ToDouble($"{Console.ReadLine():0.0}");
+            r = Console.ReadLine();
+            // Read and save an integer, double, and String to your variables.
+            Console.WriteLine(i + k);
+            // Print the sum of both integer variables on a new line.
+            Console.WriteLine((d + j).ToString("F1"));
+            // Print the sum of the double variables on a new line.
+            Console.WriteLine(s + r);
+            Console.ReadLine();
+            // Concatenate and print the String variables on a new line
+            // The 's' variable above should be printed first.
+        }
+    }
+}
