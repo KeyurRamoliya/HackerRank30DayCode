8a4eb4e815e9d98545090f525dfc4bdefa7e398a
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-06-30 21:49:15 +0530

    Day Five

---
+++ DayFive.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank30Day
+{
+    class DayFive
+    {
+        static void Main(String[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N >= 2 && N <= 20)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(N + " " + "x " + i + " = " + N * i);
+                }
+            }
+        }
+    }
+}
