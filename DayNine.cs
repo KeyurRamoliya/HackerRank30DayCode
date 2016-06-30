a86dec53be46cd3b7aae443039622fe0aa412a2e
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-06-30 21:52:14 +0530

    Day Nine

---
+++ DayNine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank30Day
+{
+    class DayNine
+    {
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n >= 2 && n <= 12)
+            {
+                long fact = factorial(n);
+                Console.WriteLine(fact);
+            }
+        }
+        private static long factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * factorial(number - 1);
+        }
+    }
+}
