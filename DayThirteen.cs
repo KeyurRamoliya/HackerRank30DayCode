66b5a5abfba9026c8199f90c6d89471f3dc6b76f
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-07-04 17:05:33 +0530

    Day Thirteen

---
+++ DayThirteen.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank30Day
+{
+    class DayThirteen
+    {
+        static void Main(String[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Book
+    {
+
+        protected String title;
+        protected String author;
+
+        public Book(String t, String a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+    }
+
+    internal class MyBook : Book
+    {
+        public int Price { get; set; }
+        private int price = 0;
+        public MyBook(string title, string author, int price) : base(title,author)
+        {
+            Price = price;
+            base.title = title;
+            base.author = author;
+        }
+
+        public override void display()
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + Price);
+        }
+    }
+}
