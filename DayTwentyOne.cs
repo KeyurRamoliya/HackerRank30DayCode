164cff648e280fbb16fcbf6b8c657f789ce3a7ba
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-07-12 21:38:32 +0530

    Day Twenty One

---
+++ DayTwentyOne.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank30Day
+{
+    class DayTwentyOne
+    {
+        static void Main(string[] args)
+        {
+            int[] vInt = new int[] { 1, 2, 3 };
+            string[] vString = new string[] { "Hello", "World" };
+
+            printArray<int>(vInt);
+            printArray<string>(vString);
+            Console.ReadLine();
+        }
+
+        private static void printArray<T>(T[] vInt)
+        {
+            foreach (var item in vInt)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
