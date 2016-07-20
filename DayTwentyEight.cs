1d9bb2f7f3e8e37d596ef81af4ae2370f9c3944f
Keyur Ramoliya <keyurramoliya19@gmail.com>
2016-07-20 15:22:17 +0530

    Day Twenty Eight

---
+++ DayTwentyEight.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace HackerRank30Day
+{
+    class DayTwentyEight
+    {
+        static void Main(String[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            List<string> names = new List<string>();
+
+            for (int a0 = 0; a0 < N; a0++)
+            {
+                string[] tokens_firstName = Console.ReadLine().Split(' ');
+                string firstName = tokens_firstName[0];
+                string emailID = tokens_firstName[1];
+
+                string pattern = @"@gmail.com$";
+                Match match = Regex.Match(emailID, pattern);
+
+                if (match.Success) { names.Add(firstName); }
+            }
+
+            names.Sort();
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
